1a174b449961687b75932c43210ba2b3e09223f9
lnkz80 <lnkz80@gmail.com>
2019-09-26 11:19:33 +0300

    3d lesson

--- js/index.js
+++ js/index.js
@@ -35,6 +35,7 @@ console.log();
 
 function startGame() {
   start.classList.add('hide');
+  gameArea.innerHTML = '';
   //the road was not so high as a page.
   //Class was added with 100vh option to div.gameArea
   gameArea.classList.add('fullPageHeight');
@@ -54,9 +55,12 @@ function startGame() {
     enemy.style.background = 'transparent url("../img/enemy' + getRandom(1, 4) + '.png") center / cover no-repeat';
     gameArea.appendChild(enemy);
   }
-
+  setting.score = 0;
   setting.start = true;
   gameArea.appendChild(car);
+  car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2;
+  car.style.top = 'auto';
+  car.style.bottom = '10px';
   setting.x = car.offsetLeft;
   setting.y = car.offsetTop;
   requestAnimationFrame(playGame);
@@ -64,6 +68,8 @@ function startGame() {
 
 function playGame() {
   if (setting.start) {
+    setting.score += setting.speed;
+    score.innerHTML = '<span>SCORE <br> ' + setting.score + '</span>';
     moveRoad();
     moveEnemy();
     if (keys.ArrowLeft && setting.x > 0) {
@@ -111,6 +117,21 @@ function moveRoad() {
 function moveEnemy() {
   let enemy = document.querySelectorAll('.enemy');
   enemy.forEach(function(item) {
+    let carRect = car.getBoundingClientRect();
+    let enemyRect = item.getBoundingClientRect();
+
+    if (
+      carRect.top <= enemyRect.bottom &&
+      carRect.right >= enemyRect.left &&
+      carRect.left <= enemyRect.right &&
+      carRect.bottom >= enemyRect.top
+    ) {
+      setting.start = false;
+      console.warn('CRASH');
+      start.classList.remove('hide');
+      start.style.top = score.offsetHeight + 'px';
+    }
+
     item.y += setting.speed * 2;
     item.style.top = item.y + 'px';
 
